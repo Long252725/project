7f253eb32163d5cdca433b32ed7132204a36bd65
long252725 <inthanhlong2006@gmail.com>
2023-11-10 21:56:59 +0700

    dsadsddsaadssd

--- src/Body/body.js
+++ src/Body/body.js
@@ -29,11 +29,11 @@ function Body() {
         handleComment();
         function sendComment(data) {
             
-            axios.post('https://test-api-n3fv.onrender.com/comments', {
-                nd: data
-            })
-                
-                
+            axios({
+                method: 'post',
+                url: 'https://test-api-n3fv.onrender.com/comments',
+                data: { nd: data }
+              })
                 .then(comment => {
                     console.log(comment.data)
                     setComment(comment.data)
