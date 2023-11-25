51ae75b4f07337fee08ca965d5ddbc89a49ca902
long252725 <inthanhlong2006@gmail.com>
2023-11-25 22:11:59 +0700

    dadcszxcs

--- src/Slide/Bill/bill.js
+++ src/Slide/Bill/bill.js
@@ -1,14 +1,31 @@
 import Bottom from '../../Bottom/bottom';
 import Header from '../../Header/header';
-import './bill.css'
+import './bill.css';
 function Bill() {
-    return ( <div className='bill'>
-        <Header/>
-        <div className='bill_cointainer'>
-            <div className='bill_cointainer_box'></div>
+    return (
+        <div className="bill">
+            <Header />
+            <div className="bill_cointainer">
+                <div className="bill_cointainer_box bill_cointainer_box_address ">
+                    <div className="bill_cointainer_box_address_title">
+                        <i class="fa-solid fa-location-dot left"></i>Địa Chỉ Nhận Hàng
+                    </div>
+                    <div className="bill_cointainer_box_address_name">Nguyên Thành Long (0858997109)</div>
+                    <div className="bill_cointainer_box_address_sdt"></div>
+                    <div className="bill_cointainer_box_address_addr">
+                        Nhà Văn Hóa, Xóm 11 Xuân Trung Nghi Đức, Xóm 11, Xã Nghi Đức, Thành Phố Vinh, Nghệ An
+                    </div>
+                </div>
+                <div className="bill_cointainer_box bill_cointainer_box_sp">
+                    <div className="bill_cointainer_box_sp_title">Sản phẩm</div>
+                    <div className="bill_cointainer_box_sp_price_title">Đơn giá</div>
+                    <div className="bill_cointainer_box_sp_sl_title">Số lượng</div>
+                    <div className="bill_cointainer_box_sp_finish_title">Thành tiền</div>
+                </div>
+            </div>
+            <Bottom />
         </div>
-        <Bottom/>
-    </div> );
+    );
 }
 
 export default Bill;
